42a1d106126f2419f63dc70786dfd5d14d5acba7
Willian Yassunaka <willianhy@gmail.com>
2018-03-19 22:43:33 -0300

    refactoring views

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/tecnologia', (req, res) => {
-	res.render("secao/tecnologia")
+app.get('/', (req, res) => {
+	res.render("home/index")
 })
 
-app.get('/', (req, res) => {
-	res.send("<html><body>Portal de notícias</body></html>")
+app.get('/formulario_inclusao_noticia', (req, res) => {
+	res.render("admin/form_add_noticia")
+})
+
+app.get('/noticias', (req, res) => {
+	res.render("noticias/noticias")
 })
 
 app.listen(3000, () => {
