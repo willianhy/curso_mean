01fb4b2a67db7511f16f70e34159c2fb1b5e6204
Willian Yassunaka <willianhy@gmail.com>
2018-03-19 22:35:46 -0300

    installing ejs

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-	res.send("<html><body>Portal de notícias</body></html>")
+app.get('/tecnologia', (req, res) => {
+	res.render("secao/tecnologia")
 })
 
-app.get('/tecnologia', (req, res) => {
-	res.send("<html><body>Notícias de tecnologia</body></html>")
+app.get('/', (req, res) => {
+	res.send("<html><body>Portal de notícias</body></html>")
 })
 
 app.listen(3000, () => {
