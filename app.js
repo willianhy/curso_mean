412840198503602bac5a01547d89ee45e66b223e
Willian Yassunaka <willianhy@gmail.com>
2018-04-11 23:22:26 -0300

    refactoring routes

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var app = require('./config/server')
-var rotaNoticias = require('./app/routes/noticias')(app)
-var rotaHome = require('./app/routes/home')(app)
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app)
 
 app.listen(3000, () => {
 	console.log('Server on')
