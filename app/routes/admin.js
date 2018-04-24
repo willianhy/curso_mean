3c39f49f533fb7d3638563d44d365b08dc74c954
Willian Yassunaka <willianhy@gmail.com>
2018-04-23 23:25:57 -0300

    saving data into db

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,16 @@
-module.exports = function(app){
-	app.get('/formulario_inclusao_noticia', (req, res) => {
+module.exports = function(application){
+	application.get('/formulario_inclusao_noticia', (req, res) => {
 		res.render("admin/form_add_noticia")
 	})
 
-	app.post('/noticias/salvar', (req, res) => {
-		var noticias = req.body
-		res.send(noticias)
+	application.post('/noticias/salvar', (req, res) => {
+		var noticia = req.body
+
+		var connection = application.config.dbConnection()
+		var noticiasModel = application.app.models.noticiasModel
+
+		noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+			res.redirect('/noticias')
+		})
 	})
 }
